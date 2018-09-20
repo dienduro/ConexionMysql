63bd1087e27ab04ed9b11283ef66c61047ec2519
dienduro <43021368+dienduro@users.noreply.github.com>
2018-09-20 17:36:18 -0500

    Add files via upload

---
+++ ConexionMysqlCorva.java
@@ -0,0 +1,14 @@
+
+package conexionmysqlcorva;
+
+import java.sql.SQLException;
+
+public class ConexionMysqlCorva {
+
+  
+    public static void main(String[] args) throws SQLException{
+        conexion conec =new conexion();
+
+    }
+    
+}
